e290bcb94ba7da942363acf93fd70748b0d397b8
helmi <helmichiha@gmail.com>
2022-07-11 16:01:53 +0200

    form (creat bot ok) on a custom path

--- createbot.py
+++ createbot.py
@@ -1,11 +1,13 @@
 
 import os
+import streamlit as st 
 
 class CreateBot :
 	def __init__(self, name_bot, secret_key, api_key):
 		self.name_bot = name_bot
 		self.api_key =api_key
-		self.secret_key	= secret_key	
+		self.secret_key	= secret_key
+			
 
 	def get_name_bot(self):
 		return self.name_bot
@@ -15,12 +17,34 @@ class CreateBot :
 	
 	def get_secret_key(self):
 		return self.secret_key
+
+
 	
-	def create_bot (self):
-		pass
+	def create_bot (self, path):
+
+		if self.name_bot ==  "Trix":
+			path_trix = path+ "/trix/"
+			st.write(f"cree un bot trix dans {path_trix} ")
+
+			if not os.path.exists("/home/helmi/backend_crypto/trix/"):
+    	   			os.makedirs(f'{path_trix}')
+			else:
+					st.error("ce bot existe déja ")
+
+		if self.name_bot == "Cocotier":
+				path_cocotier= path+ "/cocotier/"
+				st.write(f"cree un bot Cocotier dans {path_cocotier}")
+				st.write("nous comme dans le cocotier ")
+				if not os.path.exists(f"/home/helmi/backend_crypto/cocotier/{user.get_name}"):
+    	   				os.makedirs(f'{path_cocotier}')
+				else:
+						st.error("ce bot existe déja ")
+
+		return print("bot created ")
+		
         		
 class Users :
-	def __init__(self, name, email ):
+	def __init__(self, name, email):
 		self.name = name
 		self.email = email
 
--- main.py
+++ main.py
@@ -2,35 +2,31 @@ import streamlit as st
 from createbot import Users, CreateBot
 import os 
 
+
 def main():
     st.title("Create a bot ")
-   
-    
-    selection_bot = st.selectbox("Bot",  ["Trix","Cocotier"])
-    # front entry
-    name =st.text_input("Entrer your name")
-    email = st.text_input("Entrer your email ")
-    api_key = st.text_input("enter your api_key")
-    secret_key = st.text_input("enter  secret key")
+    #
+    with st.expander("inscription ", expanded=False):
+        name_robot = ["Trix","Cocotier"]
+        selection_bot = st.selectbox("Bot",  name_robot)
 
-    user = Users(name, email)
-    bot = CreateBot(selection_bot, api_key, secret_key )
+        # front entry
+        name =st.text_input("Entrer your name")
+        email = st.text_input("Entrer your email ")
+        api_key = st.text_input("enter your api_key")
+        secret_key = st.text_input("enter  secret key")
 
-       
-    if st.button('Create this bot'):     
-        #  create  bot with a personal data 
-        st.write("nom:", user.name)
-        st.write("email:", user.email)
-        st.write("secret key:", bot.secret_key)
-        st.write("secret key:", bot.api_key)
-
+        user = Users(name, email)
+        bot = CreateBot(selection_bot, api_key, secret_key)
         
+        if st.button('Create this bot'):     
+            #  create  bot with a personal data             
 
+            st.write("nom:", user.name)
+            st.write("email:", user.email)
+            st.write("secret key:", bot.secret_key)
+            st.write("secret key:", bot.api_key)
+            st.write(bot.create_bot("/home/helmi/backend_crypto"))
     
-
-
-
-
-
 if __name__ == "__main__":
     main()
