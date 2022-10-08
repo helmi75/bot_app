1fa6160427a8aec46c1ba49a784c462748225ff0
keranis <mahdihadjsassi@fsb.u-carthage.tn>
2022-10-08 17:03:02 +0100

    modification des ittérateurs avec des identification plus significatifs

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ import pandas as pd
 import plotly.express as px
 import pyautogui
 
+
 def delBot(x):
     con = ConnectBbd('localhost', '3306', 'root', 'Magali_1984', 'cryptos', 'mysql_native_password')
     con.delete_bot(x)
@@ -19,104 +20,111 @@ def main():
     st.title("Cocobots")
     authenticator = auth.auth_data()
     con = ConnectBbd('localhost', '3306', 'root', 'Magali_1984', 'cryptos', 'mysql_native_password')
-    password = None
     name, authentication_status, username = authenticator.login('Login', 'main')
     if authentication_status:
         authenticator.logout('Logout', 'main')
         st.write(f'bienvenue *{name}* à votre espace crypto ')
-        st.write(password)
         st.title('espace perso')
     elif authentication_status == False:
         st.error('Username/password is incorrect')
     elif authentication_status == None:
         st.warning('Please enter your username and password')
-    if authentication_status == True:
+    if authentication_status:
         st.write(authenticator.credentials['usernames'][username]['adresse'])
         st.write(authenticator.credentials['usernames'][username]['CP'])
 
     # creation bot
-    if authentication_status == True:
+    if authentication_status:
         with st.expander("Creat a new bot", expanded=False):
-            name_robot = ["Trix", "Cocotier"]
-            selection_bot = st.selectbox("choise your Bot", name_robot)
-            if selection_bot == "Trix":
-                # front entry
-                bot_name = st.text_input("Entrer the bot name ")
-                email = st.text_input("Entrer your email ",
-                                      value=authenticator.credentials['usernames'][username]['email'])
-                api_key = st.text_input("enter your api_key")
-                secret_key = st.text_input("enter  secret key")
-                sub_account = st.text_input("Subaccount")
-                col1, col2, col3 = st.columns(3)
-                pair_symbol = col1.text_input("FTX Pair symbol", value="ETH/USDT", placeholder="i.e  BTC/USDT")
-                trix_lenght = col2.number_input("Trix Lenght", value=9)
-                trix_signal = col3.number_input("Trix Signal", value=21)
-                stoch_top = col1.number_input("Stoch Top", value=0.88)
-                stoch_bottom = col2.number_input("Stoch Bottom", value=0.15)
-                stoch_rsi = col3.number_input("Stoch RSI", value=13)
+            try:
+                name_robot = ["Trix", "Cocotier"]
+                selection_bot = st.selectbox("choise your Bot", name_robot, key="selection_bot")
 
-            if selection_bot == "Cocotier":
-                bot_name = st.text_input("Entrer the bot name ")
-                email = st.text_input("Entrer your email ")
-                api_key = st.text_input("enter your api_key")
-                secret_key = st.text_input("enter  secret key")
+                if selection_bot == "Trix":
+                    # front entry
+                    bot_name = st.text_input("Entrer the bot name ", key="trix_name")
+                    email = st.text_input("Entrer your email ", key="email",
+                                          value=authenticator.credentials['usernames'][username]['email'])
+                    api_key = st.text_input("enter your api_key", key="api_key")
+                    secret_key = st.text_input("enter  secret key", key="secret_key")
+                    sub_account = st.text_input("Subaccount", key="sub_account")
+                    col1, col2, col3 = st.columns(3)
+                    pair_symbol = col1.text_input("FTX Pair symbol", value="ETH/USDT", placeholder="i.e  BTC/USDT", key="pair_symbol")
+                    trix_lenght = col2.number_input("Trix Lenght", value=9, key="trix_length")
+                    trix_signal = col3.number_input("Trix Signal", value=21, key="trix_signal")
+                    stoch_top = col1.number_input("Stoch Top", value=0.88, key="stoch_top")
+                    stoch_bottom = col2.number_input("Stoch Bottom", value=0.15, key="stoch_bottom")
+                    stoch_rsi = col3.number_input("Stoch RSI", value=13, key="stoch_rsi")
 
-            user = Users(name, email)
-            bot = CreateBot(user, bot_name, selection_bot, con)
+                if selection_bot == "Cocotier":
+                    bot_name = st.text_input("Entrer the bot name ", key="cocotier_name")
+                    email = st.text_input("Entrer your email ", key="cocotier_email")
+                    api_key = st.text_input("enter your api_key", key="cocotier_api_key")
+                    secret_key = st.text_input("enter  secret key", key="secret_key")
 
-            if st.button('Create this bot'):
-                #  create  bot with a personal data
-                st.write("nom:", user.name)
-                st.write("email:", user.email)
-                # encode_message(self, password)
-                # bot.encrypt_message(frenet_key, message)
-                st.write("secret key:", secret_key)
-                st.write("secret key:", api_key)
+                user = Users(name, email)
+                bot = CreateBot(user, bot_name, selection_bot, con)
 
-                bot.create__bot(
-                    selection_bot, bot_name, user.get_email(),
-                    api_key, secret_key, sub_account, pair_symbol, trix_lenght, trix_signal, stoch_top, stoch_bottom,
-                    stoch_rsi)
+                if st.button('Create this bot', key="createBot"):
+                    #  create  bot with a personal data
+                    st.write("nom:", user.name)
+                    st.write("email:", user.email)
+                    # encode_message(self, password)
+                    # bot.encrypt_message(frenet_key, message)
+                    st.write("secret key:", secret_key)
+                    st.write("secret key:", api_key)
 
-    if authentication_status == True:
+                    bot.create__bot(
+                        selection_bot, bot_name, user.get_email(),
+                        api_key, secret_key, sub_account, pair_symbol, trix_lenght, trix_signal, stoch_top,
+                        stoch_bottom,
+                        stoch_rsi)
+                    pyautogui.hotkey('ctrl', 'F5')
+            except Exception as e:
+                print("probléme")
+                print(e)
+    if authentication_status:
         with st.expander("visulation de vos bot   ", expanded=False):
             try:
-                myresult2 = con.get_balences()
-                column_names2 = ['dates', 'crypto_wallet', 'nom_bot']
-                workbook2 = pd.DataFrame(columns=column_names2)
-                for i in myresult2:
-                    workbook2.loc[len(workbook2.index)] = i
-                workbook2["dates"] = pd.to_datetime(workbook2['dates'])
-                workbook2["crypto_wallet"] = pd.to_numeric(workbook2['crypto_wallet'])
-                list_df2 = {}
-                for i in workbook2['nom_bot'].unique():
-                    l = ['dates', i]
-                    d = pd.DataFrame(columns=l)
-                    list_df2[i] = d
-                for i in workbook2.values:
-                    for j, k in list_df2.items():
-                        if (i[2] == j):
-                            k.loc[len(k.index)] = [i[0], i[1]]
-                dff2 = pd.concat(list(list_df2.values()))
-                dff2 = dff2.sort_values('dates', ignore_index=True)
-                dff2 = dff2.fillna(method='ffill')
-                dff2 = dff2.fillna(0)
-                fig2 = px.line(dff2, x="dates", y=dff2.columns, title='bots showed by date and wallet')
+                list_balences = con.get_balences()
+                balences_data_frame_columns = ['dates', 'crypto_wallet', 'nom_bot']
+                balences_data_frame = pd.DataFrame(columns=balences_data_frame_columns)
+                for balence in list_balences:
+                    balences_data_frame.loc[len(balences_data_frame.index)] = balence
+                balences_data_frame["dates"] = pd.to_datetime(balences_data_frame['dates'])
+                balences_data_frame["crypto_wallet"] = pd.to_numeric(balences_data_frame['crypto_wallet'])
+                balence_bot_name_dictionnaire = {}
+                for bot_name in balences_data_frame['nom_bot'].unique():
+                    balence_bot_name_dictionnaire_dataframe_columns = ['dates', bot_name]
+                    balence_bot_name_dictionnaire_dataframe_values = pd.DataFrame(
+                        columns=balence_bot_name_dictionnaire_dataframe_columns)
+                    balence_bot_name_dictionnaire[bot_name] = balence_bot_name_dictionnaire_dataframe_values
+                for bot in balences_data_frame.values:
+                    for balence_bot_name_dictionnaire_keys, balence_bot_name_dictionnaire_values in balence_bot_name_dictionnaire.items():
+                        if (bot[2] == balence_bot_name_dictionnaire_keys):
+                            balence_bot_name_dictionnaire_values.loc[
+                                len(balence_bot_name_dictionnaire_values.index)] = [bot[0], bot[1]]
+                balence_bots_clean_dataframe = pd.concat(list(balence_bot_name_dictionnaire.values()))
+                balence_bots_clean_dataframe = balence_bots_clean_dataframe.sort_values('dates', ignore_index=True)
+                balence_bots_clean_dataframe = balence_bots_clean_dataframe.fillna(method='ffill')
+                balence_bots_clean_dataframe = balence_bots_clean_dataframe.fillna(0)
+                fig2 = px.line(balence_bots_clean_dataframe, x="dates", y=balence_bots_clean_dataframe.columns,
+                               title='bots showed by date and wallet')
                 st.plotly_chart(fig2)
-            except:
-                pass
+            except Exception as e:
+                print(e)
         with st.expander("Gestion Des Bots   ", expanded=False):
             try:
                 bots = con.get_bots()
-                bot = [i[1] for i in bots]
+                bot = [bot_item[1] for bot_item in bots]
                 if 'my_list' not in st.session_state:
                     st.session_state.my_list = bot
 
                 for index, item in enumerate(st.session_state.my_list):
-                    emp = st.empty()
-                    col1, col2 = emp.columns([9, 4])
-                    empp = st.empty()
-                    col3, col4 = empp.columns([9, 4])
+                    emplacement = st.empty()
+                    col1, col2 = emplacement.columns([9, 4])
+                    emplacement2 = st.empty()
+                    col3, col4 = emplacement2.columns([9, 4])
 
                     if f'{item}' not in st.session_state:
                         st.session_state[f'{item}'] = False
@@ -135,9 +143,9 @@ def main():
                     if len(st.session_state.my_list) > index:
                         col1.markdown(f'Bot : **{item}**.', unsafe_allow_html=True)
                     else:
-                        emp.empty()
-            except:
-                pass
+                        emplacement.empty()
+            except Exception as e:
+                print(e)
 
 
 if __name__ == "__main__":
