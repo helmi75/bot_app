22093b2a1d7cd7d4c57a723ca399557335d3c627
keranis <mahdihadjsassi@fsb.u-carthage.tn>
2023-02-01 20:22:41 +0100

    BOTV2-141
    supprimer message d erreur sur back test cocotier + mettre bouton submit comme back test trix

--- pages/9_🧪_Back Test Cocotier.py
+++ pages/9_🧪_Back Test Cocotier.py
@@ -47,43 +47,43 @@ def main():
     client = Client()
     crypto = {}
 
+    if (st.button("Submit")):
+        for elm in market :
 
-    for elm in market :
+            #get the values from binance as they are and convert them to a dataframe we can work with
+            x = elm.lower()
 
-        #get the values from binance as they are and convert them to a dataframe we can work with
-        x = elm.lower()
+            crypto[x] = client.get_historical_klines(x.replace("/", "").upper(), delta_hour, star_time, end_time)
+            crypto[x] = pd.DataFrame(data=crypto[x],
+                                     columns=['timestamp', x[:-5] + '_open', 'high', 'low', x[:-5] + '_close', 'volume',
+                                              'close_time', 'quote_av', 'trades', 'tb_base_av', 'tb_quote_av', 'ignore'])
+            crypto[x] = crypto[x][['timestamp', x[:-5] + '_open', x[:-5] + '_close']]
+            crypto[x] = convert_time(crypto[x])
+            crypto[x] = crypto[x].astype({x[:-5] + '_open': 'float64',
+                                          x[:-5] + '_close': 'float64'
+                                          })
+            crypto[x] = crypto[x].set_index('timestamp')
 
-        crypto[x] = client.get_historical_klines(x.replace("/", "").upper(), delta_hour, star_time, end_time)
-        crypto[x] = pd.DataFrame(data=crypto[x],
-                                 columns=['timestamp', x[:-5] + '_open', 'high', 'low', x[:-5] + '_close', 'volume',
-                                          'close_time', 'quote_av', 'trades', 'tb_base_av', 'tb_quote_av', 'ignore'])
-        crypto[x] = crypto[x][['timestamp', x[:-5] + '_open', x[:-5] + '_close']]
-        crypto[x] = convert_time(crypto[x])
-        crypto[x] = crypto[x].astype({x[:-5] + '_open': 'float64',
-                                      x[:-5] + '_close': 'float64'
-                                      })
-        crypto[x] = crypto[x].set_index('timestamp')
-
-    #Correction wrong values
-    array_mauvais_shape = detection_mauvais_shape(crypto)
-    crypto = correction_shape(crypto, array_mauvais_shape)
-    for elm in array_mauvais_shape:
-        crypto[elm]['timestamp'] = generation_date(crypto[elm], int(delta_hour[:1]))
-        crypto[elm] = crypto[elm].set_index('timestamp')
-    crypto = variationN(crypto, n_i)
-    crypto = coeffMulti(crypto)
-    crypto = mergeCryptoTogether(crypto)
-    crypto, maxis = botMax(crypto)
-    crypto = botMaxVariation2(crypto, maxis)
-    crypto = coeffMultiBotMax(crypto)
-    coefMulti = coefmultiFinal(crypto)
-    plot_courbes2(coefMulti)
-    st.write(coefMulti.tail(1))
-    if st.checkbox('Voir tableau coef multi'):
-        st.write(coefMulti)
-    if st.checkbox('Voir tableau de variation'):
-        crypto = VariationFinal(crypto)
-        st.write(crypto)
+        #Correction wrong values
+        array_mauvais_shape = detection_mauvais_shape(crypto)
+        crypto = correction_shape(crypto, array_mauvais_shape)
+        for elm in array_mauvais_shape:
+            crypto[elm]['timestamp'] = generation_date(crypto[elm], int(delta_hour[:1]))
+            crypto[elm] = crypto[elm].set_index('timestamp')
+        crypto = variationN(crypto, n_i)
+        crypto = coeffMulti(crypto)
+        crypto = mergeCryptoTogether(crypto)
+        crypto, maxis = botMax(crypto)
+        crypto = botMaxVariation2(crypto, maxis)
+        crypto = coeffMultiBotMax(crypto)
+        coefMulti = coefmultiFinal(crypto)
+        plot_courbes2(coefMulti)
+        st.write(coefMulti.tail(1))
+        if st.checkbox('Voir tableau coef multi'):
+            st.write(coefMulti)
+        if st.checkbox('Voir tableau de variation'):
+            crypto = VariationFinal(crypto)
+            st.write(crypto)
 
 if __name__ == '__main__':
     main()
