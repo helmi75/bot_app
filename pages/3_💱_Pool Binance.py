34fbdc2eeb289e0fafd93b7a8b9b4cedef4b465f
Ayoub <ach.pro88@gmail.com>
2023-12-29 10:50:47 +0100

    Modify pool method binance

--- pages/3_💱_Pool Binance.py
+++ pages/3_💱_Pool Binance.py
@@ -3,6 +3,7 @@ from binance.client import Client
 from utilities.bdd_communication import ConnectBbd
 from ProjectSettings import *
 from pass_secret import mot_de_passe
+import ccxt
 
 st.set_page_config(
     page_title= page_title,
@@ -16,8 +17,9 @@ st.title("Pool Binance Pairs Symbol")
 
 con = ConnectBbd('localhost', '3306', 'root', mot_de_passe, 'cryptos', 'mysql_native_password')
 liste_crypto = con.getAllPairSymbolsBinance()[0][0].split(',')
-@st.cache_data
-def getAllPairSymbolsOfBinance():
+
+@st.cache
+def getAllPairSymbolsOfBinance_old():
     cryptoss = []
     client = Client()
     exchange_info = client.get_exchange_info()
@@ -25,6 +27,14 @@ def getAllPairSymbolsOfBinance():
         if s['symbol'].endswith('USDT'):
             cryptoss.append(s['symbol'])
     return cryptoss
+
+@st.cache
+def getAllPairSymbolsOfBinance():
+    binance = ccxt.binance()
+    symbols = list(binance.load_markets().keys())
+    symbols = [symbol.replace('/', '') for symbol in symbols if symbol.endswith('USDT')]
+    return symbols
+
 cryptoss = getAllPairSymbolsOfBinance()
 cols2 = st.columns(4)
 newCrypto = cols2[0].text_input("Write the pair symbol here!")
@@ -38,7 +48,7 @@ if (searching or st.session_state.searching):
         crr += 'USDT'
     countt = cryptoss.count(crr)
     if (countt == 0):
-        st.warning("cette crypto n’existe pas")
+        st.warning(f"La crypto {crr} n’existe pas")
     else :
         if(liste_crypto.count(crr[:-4])):
             if(cols2[2].button("Delete it from List")):
@@ -52,7 +62,7 @@ if (searching or st.session_state.searching):
                 liste_crypto.append(crr[:-4])
                 newcrr = ','.join(liste_crypto)
                 con.updatePairSymbolsBinance(newcrr)
-                st.success(f"{crr[:-4]} a été ajouté à la listse")
+                st.success(f"{crr[:-4]} a été ajouté à la liste")
 
 
 
