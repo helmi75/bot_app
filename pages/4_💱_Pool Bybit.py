930b974147488b548788ae5561a44c402e6c43bc
Ayoub <ach.pro88@gmail.com>
2023-12-29 10:51:50 +0100

    Modify pool method bybit

--- pages/4_💱_Pool Bybit.py
+++ pages/4_💱_Pool Bybit.py
@@ -4,6 +4,7 @@ from utilities.bdd_communication import ConnectBbd
 from pass_secret import mot_de_passe
 from binance.client import Client
 from ProjectSettings import  *
+import ccxt
 
 st.set_page_config(
     page_title= page_title,
@@ -16,8 +17,8 @@ st.title("Pool Bybit Pairs Symbol")
 
 con = ConnectBbd('localhost', '3306', 'root', mot_de_passe, 'cryptos', 'mysql_native_password')
 liste_crypto = con.getAllPairSymbolsBybit()[0][0].split(',')
-@st.cache_data
-def getAllPairSymbolsOfBybit():
+@st.cache
+def getAllPairSymbolsOfBybit_old():
     cryptoss = []
     url = 'https://api.bybit.com/v2/public/symbols'
     response = requests.get(url)
@@ -27,8 +28,8 @@ def getAllPairSymbolsOfBybit():
             cryptoss.append(s['name'])
     return cryptoss
 
-@st.cache_data
-def getAllPairSymbolsOfBinance():
+@st.cache
+def getAllPairSymbolsOfBinance_old():
     cryptoss = []
     client = Client()
     exchange_info = client.get_exchange_info()
@@ -37,41 +38,59 @@ def getAllPairSymbolsOfBinance():
             cryptoss.append(s['symbol'])
     return cryptoss
 
-cryptoss = getAllPairSymbolsOfBinance()
-cryptosss = getAllPairSymbolsOfBybit()
-cols2 = st.columns(4)
-newCrypto = cols2[0].text_input("Write the pair symbol here!")
-searching = cols2[3].button("Search")
-if 'searching' not in st.session_state:
-    st.session_state.searching = False
-if (searching or st.session_state.searching):
-    st.session_state.searching = True
-    crr = newCrypto.upper().replace('/', '')
-    if not (len(crr) > 4 and crr[-4:] == 'USDT'):
-        crr += 'USDT'
-    countt = cryptoss.count(crr)
-    counttt = cryptosss.count(crr)
-    if (countt == 0 or counttt == 0):
-        st.warning("cette crypto n’existe pas")
-    else :
-        if(liste_crypto.count(crr[:-4])):
-            if(cols2[2].button("Delete it from List")):
-                liste_crypto.remove(crr[:-4])
-                newcrr = ','.join(liste_crypto)
-                con.updatePairSymbolsBybit(newcrr)
-                st.success(f"{crr[:-4]} a été supprimé de la liste")
+@st.cache
+def getAllPairSymbolsOfBybit():
+    url = "https://api.bybit.com/v2/public/symbols"
+    response = requests.get(url)
+    data = response.json()
+    symbols = [symbol["name"] for symbol in data["result"]]
+    return symbols
 
-        else:
-            if (cols2[2].button("Add it to List")):
-                liste_crypto.append(crr[:-4])
-                newcrr = ','.join(liste_crypto)
-                con.updatePairSymbolsBybit(newcrr)
-                st.success(f"{crr[:-4]} a été ajouté à la listse")
+@st.cache
+def getAllPairSymbolsOfBinance():
+    binance = ccxt.binance()
+    symbols = list(binance.load_markets().keys())
+    symbols = [symbol.replace('/', '') for symbol in symbols if symbol.endswith('USDT')]
+    return symbols
 
+# Fonction pour obtenir les symboles spot de Bybit
+def get_bybit_spot_symbols():
+    bybit = ccxt.bybit({
+        'apiKey': "YAhlHZJaBFFr6ixX33",
+        'secret': "5zMAhtMZ6MXxtR2p5tcU15x3LyRkYR2jh8Px",
+        'enableRateLimit': True,
+    })
+    markets = bybit.load_markets()
+    spot_symbols = [symbol.replace('/', '') for symbol, market in markets.items() if (market['type'] == 'spot') and (symbol.endswith('USDT')) and (not any(char.isdigit() for char in symbol))]
+  
+    return spot_symbols
 
+# Fonction pour mettre à jour la liste des cryptomonnaies communes
+def update_common_symbols():
+    binance_symbols = getAllPairSymbolsOfBinance()
+    #bybit_symbols = getAllPairSymbolsOfBybit()
+    bybit_symbols = get_bybit_spot_symbols()
+    
+    # Convertir les symboles en ensembles pour faciliter l'intersection
+    binance_set = set([symbol[:-4] for symbol in binance_symbols])
+    bybit_set = set([symbol[:-4] for symbol in bybit_symbols])
 
+    # Trouver les symboles communs
+    common_symbols = binance_set.intersection(bybit_set)
 
-cols3 = st.columns(7)
-for i in range(len(liste_crypto)):
-    cols3[i%7].write(liste_crypto[i])
+    # Convertir de nouveau au format d'origine si nécessaire
+    common_symbols_format_original = sorted([f"{symbol}" for symbol in common_symbols])
 
+    return common_symbols_format_original
+
+# Bouton pour actualiser la liste des cryptomonnaies
+if st.button("Actualiser la liste des cryptos"):
+    liste_crypto = update_common_symbols()
+    newcrr = ','.join(liste_crypto)
+    con.updatePairSymbolsBybit(newcrr)
+    st.success(f"La liste a été mise à jour avec succès")
+
+# Afficher la liste mise à jour dans des colonnes
+cols = st.columns(7)
+for i, symbol in enumerate(liste_crypto):
+    cols[i % 7].write(symbol)
